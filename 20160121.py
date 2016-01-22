1742475690e4b8a67c6d49db8bfde37e962f1879
Rebeccah Duvoisin <rduvoisin@gmail.com>
2016-01-22 14:52:33 -0600

    First commit from Debian VM

---
+++ 20160121.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.plot as plt
+print("hello, there!)
+crime = pd.read_csv('../pa1/dec_2015_crimes.csv', parse_dates=['Date'])
+
