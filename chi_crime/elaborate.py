1dda53728e9fa0fbce7ffcb8369e176be8a70112
Rebeccah Duvoisin <rduvoisin@gmail.com>
2016-02-02 15:50:25 -0600

    PA2 Distance formulation in Question 4

--- chi_crime/elaborate.py
+++ chi_crime/elaborate.py
@@ -815,20 +815,6 @@ fig.savefig(doc)
 #
 # Download the police stations data.
 #
-stations = pd.read_csv('Police_Stations.csv')
-# (a) Extract the latitudes and longitudes of the police stations
-# (found in the ADDRESS column) as floats into their own columns called
-# 'Station Latitude' and 'Station Longitude', respectively.
-station_lat = 'Station Latitude'
-station_lon = 'Station Longitude'
-
-#
-# (b) Join the crime data with the stations on police district.
-# Hint: the station district is a text field (because one of them is 'Headquarters')
-# so you'll need to convert the crime district to the same.
-dem_crime_map[district] = dem_crime_map[district].astype(str)
-# create coordinates objects out of crime lat and lon
-crime_coords = 'Crime Coordinates'
 def make_coordinates(latloncol):
     if isinstance(latloncol, str):
         y = latloncol[1:len(latloncol)-1][:]
@@ -836,19 +822,45 @@ def make_coordinates(latloncol):
         laty = float(z)
         x = y[y.index(',')+2:][:]
         longy = float(x)
-        newcoord = Coordinates()
+        newcoord = Coordinates(laty, longy)
         newcoord.latitude = laty
         newcoord.longitude = longy
         return newcoord
     elif isinstance(latloncol, int):
-        newcoord = Coordinates()
+        if latloncol == 0:
+            return False
+        newcoord = Coordinates(latloncol, latloncol)
         newcoord.latitude = latloncol
         newcoord.longitude = latloncol
         return newcoord
+    else:
+        return False
 
+stations = pd.read_csv('Police_Stations.csv')
+# (a) Extract the latitudes and longitudes of the police stations
+# (found in the ADDRESS column) as floats into their own columns called
+# 'Station Latitude' and 'Station Longitude', respectively.
+station_lat = 'Station Latitude'
+station_lon = 'Station Longitude'
+station_coords = 'Station Coordinates'
+station_latlon = 'Station Lat_Lon'
+stations[station_latlon] = stations['LOCATION']
+stations[station_latlon] = stations[station_latlon].apply(lambda x: x[x.index('('):x.index(')')+1])
+stations[station_coords] = stations[station_latlon]
+stations[station_coords] = stations[station_coords].apply(make_coordinates)
+
+# create likewise coordinates objects out of crime lat and lon
+crime_coords = 'Crime Coordinates'
 dem_crime_map[crime_coords] = dem_crime_map[lat_lon]
 dem_crime_map[crime_coords] = dem_crime_map[crime_coords].apply(make_coordinates)
-#
+
+# (b) Join the crime data with the stations on police district.
+# Hint: the station district is a text field (because one of them is 'Headquarters')
+# so you'll need to convert the crime district to the same.
+dem_crime_map[district] = dem_crime_map[district].astype(str)
+stations = stations.rename(columns={'DISTRICT':district})
+dem_crime_map = dem_crime_map.merge(stations, on=district)
+
 # (c) Define a function which calculates the distance in kilometers
 # between two points (latitude, longitude) using the Pythagorean theorem.
 #
@@ -857,8 +869,11 @@ dem_crime_map[crime_coords] = dem_crime_map[crime_coords].apply(make_coordinates
 # This is the scale factor for these coordinates near Chicago.
 # Note this method is approximate because the scale factor varies
 # from point to point (i.e. the Earth is not flat!).
-#
-# (d) Calculate the distance between each crime and its district police station. Hint: If your answer to (c) is of the form
+
+# This is done from with the Coordinates object
+
+# (d) Calculate the distance between each crime and its district
+# police station. Hint: If your answer to (c) is of the form
 #
 # def distance(row):
 # crime_lat = row['Latitude']
@@ -868,5 +883,17 @@ dem_crime_map[crime_coords] = dem_crime_map[crime_coords].apply(make_coordinates
 # ...
 # Then you can simply do
 # df.apply(distance, axis=1)
-#
-# (e) Plot a histogram of crime count against distance to district police station. Summarize the relationship in words.
+def get_distance_to(row):
+    if row[crime_coords] and row[station_coords]:
+        return row[crime_coords].distance_to(row[station_coords]) / 1000
+    else:
+        return None
+
+distance = 'Distance to Station'
+dem_crime_map[distance] = dem_crime_map[crime_coords]
+# .apply(.distance_to([crime_coords]))
+# , dem_crime_map[station_coords]))
+dem_crime_map[distance] = dem_crime_map.apply(get_distance_to, axis=1)
+# dem_crime_map[distance] = dem_crime_map[distance] / 1000
+# (e) Plot a histogram of crime count against distance to district police station.
+# Summarize the relationship in words.
